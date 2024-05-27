125e045307755d2952e8a35aab542b897e646755
pranit sarode <pranitsarode2003@gmail.com>
2024-05-27 18:10:34 +0530

    1 msg

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const playButton = document.querySelector('.play-btn');
+    playButton.addEventListener('click', () => {
+        alert('Play button clicked!');
+    });
+});
